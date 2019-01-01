b25b835fe4e4fd2f62de0e4854712d6bdbc2e99b
Stephen Rufle <srufle@gmail.com>
2018-12-31 23:22:52 -0700

    Adding tmux config

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,4 +4,5 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
